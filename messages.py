648b9f3a9523f66bffaa6e25ccc64b25c48c905f
nyx <loool1912@web.de>
2022-03-01 18:51:22 +0100

    fiddling

--- messages.py
+++ messages.py
@@ -3,4 +3,6 @@ from telegram.ext import CallbackContext
 
 
 def add_footer_meme(update: Update, context: CallbackContext):
-    update.message.caption = update.message.caption + "🔰 Subscribe to @MilitaerMemes for more"
+    print(update)
+
+    update.message.edit_caption(update.message.caption + "🔰 Subscribe to @MilitaerMemes for more")
