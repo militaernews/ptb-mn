d6008351547962799b95a6b0f3eb9703b73f57fa
nyx <loool1912@web.de>
2022-03-01 19:39:49 +0100

    fiddling

--- messages.py
+++ messages.py
@@ -7,6 +7,11 @@ from config import GROUP_MAIN
 def add_footer_meme(update: Update, context: CallbackContext):
     print(update)
 
-    update.channel_post.edit_caption(update.channel_post.caption + "\n\n🔰 Subscribe to @MilitaerMemes for more")
+    if update.channel_post.caption is not None:
+        original_caption = update.channel_post.caption
+    else:
+        original_caption = ''
+
+    update.channel_post.edit_caption(original_caption + "\n\n🔰 Subscribe to @MilitaerMemes for more!")
 
     update.channel_post.forward(chat_id=GROUP_MAIN)
