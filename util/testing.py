1f1e0358f86441386d7bc0de6c9c3dc84d1674d6
Felix Huber <felix.huber@ametras.com>
2022-07-07 16:49:21 +0200

    fix

--- util/testing.py
+++ util/testing.py
@@ -2,24 +2,27 @@ from telegram import Update
 from telegram.ext import CallbackContext
 
 from messages import handle_url
-from util.translation import flag_to_hashtag, translate_message
 
 
 def flag_to_hashtag_test(update: Update, context: CallbackContext):
     print("-------\n\nTEST\n\n-------")
-    update.message.reply_text("flag to hashtag -- TR")
+    update.message.reply_text("-------\n\nTEST\n\n-------")
+    try:
 
-    handle_url(update, context)
+        handle_url(update, context)
+    except Exception:
+        update.message.reply_text("-------\n\nTEST FAIL\n\n-------")
+        pass
 
-    update.message.reply_text("Test done")
+    update.message.reply_text("-------\n\nTEST DONE\n\n-------")
 
-    #update.message.reply_text(
+    # update.message.reply_text(
     #    flag_to_hashtag(update.message.text_html_urled, "tr"))
 
-   # update.message.reply_text("deepl -- de -- formality more")
-   # update.message.reply_text(
-   #     translate_message("de", update.message.text_html_urled))
+# update.message.reply_text("deepl -- de -- formality more")
+# update.message.reply_text(
+#     translate_message("de", update.message.text_html_urled))
 
-   # update.message.reply_text("deepl -- en-us")
-   # update.message.reply_text(
-   #     translate_message("en-us", update.message.text_html_urled))
+# update.message.reply_text("deepl -- en-us")
+# update.message.reply_text(
+#     translate_message("en-us", update.message.text_html_urled))
