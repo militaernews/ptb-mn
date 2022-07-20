d7fc5d49295fb1dc3c56ae220055fdf7192c14dc
pxnx <loool1912@web.de>
2022-07-20 06:44:13 +0200

    fiddling

--- messages.py
+++ messages.py
@@ -18,8 +18,9 @@ def get_replies(bot_data, msg_id: str):
     print("Trying to get bot_data ------------------")
     print(bot_data)
     print("-------------------------")
-    print(bot_data[bot_data[msg_id]["reply"]])
+
     if "reply" in bot_data[msg_id]:
+        print(bot_data[bot_data[msg_id]["reply"]])
         return bot_data[bot_data[msg_id]["reply"]]["langs"]
 
     return None
