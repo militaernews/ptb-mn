d1619faac570eea1a78a73b0a32601d62d3dd88e
pxnx <loool1912@web.de>
2022-08-19 10:02:17 +0200

    bingo

--- dev/playground.py
+++ dev/playground.py
@@ -1,28 +1,32 @@
 import traceback
 
 from telegram import Update
-from telegram.ext import ContextTypes, CallbackContext
+from telegram.ext import ContextTypes
+from wand.image import Image
 
-from util.translation import translate_message
+from messages import bingo
 
 
-async def flag_to_hashtag_test(update: Update, context:ContextTypes.DEFAULT_TYPE):
+async def flag_to_hashtag_test(update: Update, context: ContextTypes.DEFAULT_TYPE):
     print("-------\n\nTEST\n\n-------")
     await update.message.reply_text("-------\n\nTEST\n\n-------")
     try:
         print("--")
 
-       # await update.message.reply_text(translate_message("de", update.message.text_html_urled, None))
+        # await update.message.reply_text(translate_message("de", update.message.text_html_urled, None))
 
-        chat = await context.bot.get_chat(update.message.from_user.id)
+        with open("dev/bitmap.svg", "rb") as f:
+            with Image(blob=f.read(), format="svg") as image:
+                png_image = image.make_blob("png")
 
-        await update.message.reply_text(chat.username)
+                await update.message.reply_photo(photo=png_image)
 
-        print("--")
+        if "bingo" not in context.bot_data:
+            context.bot_data["bingo"] = bingo.generate_bingo_field()
+        bingo.set_checked(update.message.text, context.bot_data["bingo"])
 
-        user =  await context.bot.get_chat("n6y9x")
+        await update.message.reply_text(str(bingo.check_win(context.bot_data["bingo"])))
 
-        await update.message.reply_text(str(user.id))
 
     except Exception as e:
         await update.message.reply_text("-------\n\nTEST FAIL\n\n-------")
---
+++ messages/bingo.py
@@ -0,0 +1,192 @@
+import random
+from dataclasses import dataclass
+from typing import List
+
+import numpy as numpy
+import svgwrite
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ENTRIES = [
+    "Israel",
+    "Baerbock verurteilt",
+    "Lauterbach warnt",
+    "China",
+    "wirkungsvoll",
+    "Biden",
+    "Schwurbler",
+    "böse NATO",
+    "Wunderwaffe",
+    "Lindner verspricht",
+    "SDF/YPG",
+    "Hartz IV",
+    "Atomkrieg",
+    "kampferprobt",
+    "Spinner",
+    "Weltmacht",
+    "Kokain",
+    "Entnazifizieren",
+    "HIMARS",
+    "Krim",
+    "Russland droht",
+    "Biolabor",
+    "Schlangeninsel",
+    "Faschist",
+    "Nazi",
+    "Britischer Geheimdienst",
+    "Azov",
+    "Alina",
+    "Erdogan",
+    "Korruption",
+    "Konvention",
+    "Lufthohheit",
+    "Taiwan",
+    "Frontverlauf",
+    "Kinzhal",
+    "Meinung",
+    "Lukashenko",
+    "Impfung",
+    "Reichsbürger",
+    "Trump",
+    "Schwarzmarkt",
+    "einsatzbereit",
+    "Putin macht bald ernst",
+    "Militärexperte",
+    "Zweiter Weltkrieg",
+    "Palästina",
+    "Verluste",
+    "Logistik",
+    "Stalingrad",
+    "Barbarossa",
+    "Wehrmacht",
+    "Unfall",
+    "Ivan",
+    "NATO-Osterweiterung",
+    "Vorstoß",
+    "T-14",
+    "Armata",
+    "Kämpf doch selbst",
+    "Propaganda",
+    "Taliban",
+    "Wahrheit",
+    "Aber die USA",
+    "Ukraine beschießt Zivilisten",
+    "Munitionsdepot",
+    "Selenski fordert",
+    "Man muss verhandeln",
+    "Gas",
+    "Bayraktar-TB2",
+    "Quelle?",
+    "Hostomel",
+    "Elite",
+    "8 Jahre",
+    "Genozid",
+    "seit 2014"
+]
+
+
+@dataclass
+class BingoField:
+    text: str
+    checked: bool = False
+
+
+def generate_bingo_field():
+    random.shuffle(ENTRIES)
+
+    outer = list()
+
+    #   for x in range(1, 4):
+    #
+    #     inner = list()
+    #    for y in range(1, 4):
+    #       inner.append(BingoField(ENTRIES[x + y]))
+    #
+    # outer.append(inner)
+
+    for x in range(1, 5 * 5, 5):
+        inner = list()
+        print(x)
+
+        for y in ENTRIES[x:x + 5]:
+            print(y)
+            inner.append(BingoField(y))
+
+        outer.append(inner)
+
+    print(outer)
+    return outer
+
+
+def check_win(fields: List[List[BingoField]]):
+    # horizontal condition
+    for row in fields:
+        if all(item.checked for item in row):
+            return True
+
+    # vertical condition
+    for column in zip(*fields):
+        if all(item.checked for item in column):
+            return True
+
+    return False
+
+
+def set_checked(word: str, fields: List[List[BingoField]]):
+    print(list(numpy.array(fields).flat))
+    for item in list(numpy.array(fields).flat):
+        if item.text == word:
+            item.checked = True
+            print("check")
+
+
+# "<text style='font-size:3.175px;fill:#000000;stroke-width:0.264583' x='42.4465' y='32.978203' id='text277'>Test2</text>"
+
+dwg = svgwrite.Drawing('test.svg', profile='tiny', size=(600, 600))
+dwg.add(dwg.line((0, 0), (100, 40), stroke=svgwrite.rgb(10, 10, 16, '%')))
+dwg.add(dwg.text('Test', insert=(0, 8), fill='red'))
+dwg.save()
+
+canvas_size = 1080
+field_size = 3
+
+svg = f"""<?xml version='1.0' encoding='UTF-8' standalone='no'?>
+<svg
+   width='{canvas_size}'
+   height='{canvas_size}'
+   viewBox='0 0 {canvas_size} {canvas_size}'
+   version='1.1'
+   xmlns='http://www.w3.org/2000/svg'
+   xmlns:svg='http://www.w3.org/2000/svg'>"""
+
+line_width = 6
+field_width = int(canvas_size / field_size)
+print(field_width)
+print(field_width-int(line_width/2))
+print(line_width/2)
+print("---")
+
+field_nr = 0
+
+for i in range(field_width-int(line_width/2), canvas_size, field_width):
+    print(i)
+    svg += f"""
+    <rect
+       style="fill:#1884cc;fill-opacity:1"
+       width="{canvas_size}"
+       height="6"
+       x="0"
+       y="{i+field_nr*int(line_width/2) }" />
+    <rect
+       style="fill:#1884cc;fill-opacity:1"   
+       width="6"
+       height="{canvas_size}"
+       x="{i +field_nr*int(line_width/2) }"
+       y="0" />
+"""
+    field_nr += 1
+
+svg += "</svg>"
+
+print(svg)
