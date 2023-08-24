32e1081f819be2526d2155bdffbf34a76608c9b0
pentexnyx <loool1912@web.de>
2023-08-24 20:29:33 +0200

    promo with img

--- messages/private/promo.py
+++ messages/private/promo.py
@@ -9,116 +9,126 @@ from data.db import insert_promo
 from data.lang import all_langs
 
 
-async def is_member( context: CallbackContext, user_id:int, channel_index:int):
-    check =await context.bot.getChatMember(all_langs[channel_index].channel_id, user_id)
+async def is_member(context: CallbackContext, user_id: int, channel_index: int):
+    check = await context.bot.getChatMember(all_langs[channel_index].channel_id, user_id)
 
     print(f"check: {check} -- id {all_langs[channel_index].channel_id}")
 
-    return type(check)  in  (ChatMemberMember,ChatMemberOwner,ChatMemberAdministrator)
+    return type(check) in (ChatMemberMember, ChatMemberOwner, ChatMemberAdministrator)
+
 
 async def start_promo(update: Update, context: CallbackContext):
-
-    data =update.message.text.split(" ")[1].split("_")[1:]
+    data = update.message.text.split(" ")[1].split("_")[1:]
 
     cb_data = fr"promo_{data[0]}"
     if len(data) == 2:
         if int(data[1]) == update.message.from_user.id:
-            return await update.message.reply_text( #util.translation.translate()
+            return await update.message.reply_text(  # util.translation.translate()
                 "Du kannst deinen eigenen Link nicht verwenden. Schicke ihn gerne an jemand anderen 😉")
 
         cb_data += f"_{data[1]}"
 
     if await is_member(context, update.message.from_user.id, int(data[0])):
 
-        payload = (                   f"Wir von MilitärNews feiern unseren dritten Geburtstag und verlosen Telegram Premium an unsere Abonnenten 🎉\n\n"
-                   f"Damit hast du Zugriff auf eine Reihe toller Features wie mehr Zeichenlimits für Nachrichten, schnellere Downloads, automatische Übersetzung von Chats oder animierte Emojis!\n\n"
-                   f"Klicke hier um mitzumachen!\n\n"
-                   f"👉🏻 https://t.me/militaernews_posting_bot?start=promo_{data[0]}_{update.message.from_user.id}")
-
-        await update.message.reply_photo(open("res/img/mn-tg-promo-de.png","rb"),
-            "Danke für deine Teilnahme! 😊\n\n"
-            "Wenn du deine Gewinnchancen erhöhen willst, dann teile deinen persönlichen Einladungslink. Mehr Lose, höhere Gewinnchance! 💪😎",
-            reply_markup=InlineKeyboardMarkup.from_button(InlineKeyboardButton(
-                "🍀 Chancen steigern!",
-                url=f"https://t.me/share/url?url=Hey!&text={payload}"
-            ))
-        )
-
-    else:
-        await update.message.reply_photo(open("res/img/mn-tg-promo-de.png","rb"),
-            "Damit du an der Verlosung zum Jubiläum von MilitärNews teilnehmen kannst, musst du zuerst den Kanal abonnieren.\n\n"
-            "Klicke einfach auf diesen Link:\n\n"
-            "<b>https://t.me/militaernews</b>\n\n"
-            "————————————\n\n"
-            "Wenn du abonniert hast, dann verifiziere deine Teilnahme an der Verlosung mit einem Klick auf den Button 😊",
-            reply_markup=InlineKeyboardMarkup.from_button(InlineKeyboardButton(
-                "✅ Teilnehmen",
-                callback_data=cb_data
-            ))
-        )
-
-
-
-
-
-
-
-
-
-
-async def verify_promo(update: Update, context: CallbackContext):
-
-
-    data = update.callback_query.data.split("_")[1:]
-    print(update)
-    print(data)
-
-    if await is_member(context,update.callback_query.from_user.id, int(data[0])):
-        print("is member")
-
-        if len(data) ==2:
-           promo_id = int(data[1])
-        else:
-            promo_id=None
-
-
-        res = insert_promo(update.callback_query.from_user.id, all_langs[int(data[0])].channel_id, promo_id)
-
-        if res is None:
-            await update.callback_query.edit_message_text(
-                f"Du bist bereits über einen Promo-Link beigetreten. Hier nochmal dein Promo-Link:",
-                reply_markup=InlineKeyboardMarkup.from_button(InlineKeyboardButton(
-                    "Teilen",
-                    url=f"https://t.me/share/url?url=https://t.me/militaernews_posting_bot?start=promo_{data[0]}_{update.callback_query.from_user.id}&text=Bitte%20komm"
-                ))
-            )
-
         payload = (
             f"Wir von MilitärNews feiern unseren dritten Geburtstag und verlosen Telegram Premium an unsere Abonnenten 🎉\n\n"
             f"Damit hast du Zugriff auf eine Reihe toller Features wie mehr Zeichenlimits für Nachrichten, schnellere Downloads, automatische Übersetzung von Chats oder animierte Emojis!\n\n"
             f"Klicke hier um mitzumachen!\n\n"
-            f"👉🏻 https://t.me/militaernews_posting_bot?start=promo_{data[0]}_{update.message.from_user.id}")
+            f"👉🏻 https://t.me/militaernews_posting_bot?start=promo_{data[0]}_")
 
-        await update.callback_query.edit_message_text(
-                                             "Danke für deine Teilnahme! Der Nutzer von dem du diesen Link erhalten hast, bekommt zur Belohnung ein zusätzliches Los! 😊\n\n"
-                                             "Wenn du deine eigenen Gewinnchancen erhöhen willst, dann teile deinen persönlichen Einladungslink. Mehr Lose, höhere Gewinnchance! 💪😎",
-                                             reply_markup=InlineKeyboardMarkup.from_button(InlineKeyboardButton(
-                                                 "🍀 Chancen steigern!",
-                                                 url=f"https://t.me/share/url?url=Hey!&text={payload}"
-                                             ))
-                                             )
+        await update.message.reply_photo(open("res/img/mn-tg-promo-de.png", "rb"),
+                                         "Danke für deine Teilnahme! 😊\n\n"
+                                         "Wenn du deine Gewinnchancen erhöhen willst, dann teile deinen persönlichen Einladungslink. Mehr Lose, höhere Gewinnchance! 💪😎",
+                                         reply_markup=InlineKeyboardMarkup.from_button(InlineKeyboardButton(
+                                             "🍀 Chancen steigern!",
+                                             url=f"https://t.me/share/url?url=Hey!&text={payload}{update.message.from_user.id}"
+                                         ))
+                                         )
 
-
-        if promo_id is not None:
+        if len(data) == 2:
             try:
-                await context.bot.send_message(promo_id, "Ein Nutzer hat uns über deinen Link abonniert! Zur Belohnung erhältst du ein weiteres Los für unsere Jubiläums-Verlosung 🎉\n\n"
-                                                         "Teile deinen Link mit weiteren Leuten für eine höhere Gewinnchance 😉"
+                await context.bot.send_message(data[1],
+                                               "Ein Nutzer hat uns über deinen Link abonniert! Zur Belohnung erhältst du ein weiteres Los für unsere Jubiläums-Verlosung 🎉\n\n"
+                                               "Teile deinen Link mit weiteren Leuten für eine höhere Gewinnchance 😉"
                                                ,
                                                reply_markup=InlineKeyboardMarkup.from_button(InlineKeyboardButton(
                                                    "Teilen",
-                                                   url=f"https://t.me/share/url?url=https://t.me/militaernews_posting_bot?start=promo_{data[0]}_{promo_id}&text=Bitte%20komm"
+                                                   url=f"https://t.me/share/url?url=Hey!&text={payload}{data[1]}"
+
                                                ))
                                                )
+            except Exception as e:
+                logging.error(f"User blocked Bot. {e}")
+                pass
+
+    else:
+        await update.message.reply_photo(open("res/img/mn-tg-promo-de.png", "rb"),
+                                         "Damit du an der Verlosung zum Jubiläum von MilitärNews teilnehmen kannst, musst du zuerst den Kanal abonnieren.\n\n"
+                                         "Klicke einfach auf diesen Link:\n\n"
+                                         "<b>https://t.me/militaernews</b>\n\n"
+                                         "————————————\n\n"
+                                         "Wenn du abonniert hast, dann verifiziere deine Teilnahme an der Verlosung mit einem Klick auf den Button 😊",
+                                         reply_markup=InlineKeyboardMarkup.from_button(
+                                             InlineKeyboardButton("✅ Teilnehmen",  callback_data=cb_data )
+                                         )
+                                         )
+
+
+async def verify_promo(update: Update, context: CallbackContext):
+    data = update.callback_query.data.split("_")[1:]
+    print(update)
+    print(data)
+
+    if await is_member(context, update.callback_query.from_user.id, int(data[0])):
+        print("is member")
+
+        if len(data) == 2:
+            promo_id = int(data[1])
+        else:
+            promo_id = None
+
+        res = insert_promo(update.callback_query.from_user.id, all_langs[int(data[0])].channel_id, promo_id)
+
+        payload = ("https://t.me/share/url?url=Hey!&text=\n\n"
+                   f"Wir von MilitärNews feiern unseren dritten Geburtstag und verlosen Telegram Premium an unsere Abonnenten 🎉\n\n"
+                   f"Damit hast du Zugriff auf eine Reihe toller Features wie mehr Zeichenlimits für Nachrichten, schnellere Downloads, automatische Übersetzung von Chats oder animierte Emojis!\n\n"
+                   f"Klicke hier um mitzumachen!\n\n"
+                   f"👉🏻 https://t.me/militaernews_posting_bot?start=promo_{data[0]}_")
+
+        if res is None:
+            await update.callback_query.edit_message_caption(
+                f"Du bist bereits über den Link eines anderen Nutzers beigetreten. Damit es fair bleibt, kann jede Person nur ein Los für einen anderen Benutzer generieren. Sonst würde ja jemand stundenlang nur auf den Button tippen um einen anderen Nutzer mit tausenden Losen zu belohnen. 😉\n\n"
+                f"Hier nochmal dein Promo-Link:",
+                reply_markup=InlineKeyboardMarkup.from_button(InlineKeyboardButton(
+                    "🍀 Chancen steigern!",
+                    url=f"{payload}{update.message.from_user.id}"
+                ))
+            )
+
+        await update.callback_query.edit_message_caption(
+            "Danke für deine Teilnahme! Der Nutzer von dem du diesen Link erhalten hast, bekommt zur Belohnung ein zusätzliches Los! 😊\n\n"
+            "Wenn du deine eigenen Gewinnchancen erhöhen willst, dann teile deinen persönlichen Einladungslink. Mehr Lose, höhere Gewinnchance! 💪😎",
+            reply_markup=InlineKeyboardMarkup.from_button(InlineKeyboardButton(
+                "🍀 Chancen steigern!",
+                url=f"{payload}{update.message.from_user.id}"
+            ))
+        )
+
+        if promo_id is not None:
+            payload = ("https://t.me/share/url?url=Hey!&text=\n\n"
+                       f"Wir von MilitärNews feiern unseren dritten Geburtstag und verlosen Telegram Premium an unsere Abonnenten 🎉\n\n"
+                       f"Damit hast du Zugriff auf eine Reihe toller Features wie mehr Zeichenlimits für Nachrichten, schnellere Downloads, automatische Übersetzung von Chats oder animierte Emojis!\n\n"
+                       f"Klicke hier um mitzumachen!\n\n"
+                       f"👉🏻 https://t.me/militaernews_posting_bot?start=promo_{data[0]}_")
+
+            try:
+                await context.bot.send_message(promo_id,
+                                               "Ein Nutzer hat uns über deinen Link abonniert! Zur Belohnung erhältst du ein weiteres Los für unsere Jubiläums-Verlosung 🎉\n\n"
+                                               "Teile deinen Link mit weiteren Leuten für eine höhere Gewinnchance 😉",
+                                               reply_markup=InlineKeyboardMarkup.from_button(InlineKeyboardButton(
+                                                   "Teilen",
+                                                   url=f"{payload}{promo_id}"
+                                               )))
             except Exception as e:
                 logging.error(f"User blocked Bot. {e}")
                 pass
@@ -128,6 +138,3 @@ async def verify_promo(update: Update, context: CallbackContext):
     else:
         print("not member")
         await update.callback_query.answer(f"Du musst zuerst dem Kanal beitreten 😜")
-
-
-
