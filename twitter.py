d0721e261a5ad0a1936b7dc32e7fe5dafb12181b
nyx <loool1912@web.de>
2024-06-23 12:46:05 +0200

    option to deactive twitter

--- twitter.py
+++ twitter.py
@@ -19,6 +19,8 @@ access_token = os.getenv("ACCESS_KEY")
 access_secret = os.getenv("ACCESS_SECRET")
 bearer = os.getenv("BEARER")
 
+ACTIVE = False
+
 client = tweepy.Client(
     consumer_key=consumer_key,
     consumer_secret=consumer_secret,
@@ -37,6 +39,8 @@ TWEET_LENGTH = 280
 
 
 def upload_media(files):
+    if not ACTIVE:
+        return
     media_ids = []
     for file in files:
         res = api.media_upload(file)
@@ -45,6 +49,9 @@ def upload_media(files):
 
 
 def create_tweet(text, media_ids=None):
+    if not ACTIVE:
+        return
+
     text = html.fromstring(text).text_content().strip()
     try:
         client.create_tweet(text=text.replace("\n", " ").replace("  ", " ")[:TWEET_LENGTH], media_ids=media_ids)
@@ -53,6 +60,8 @@ def create_tweet(text, media_ids=None):
 
 
 async def tweet_file(text: str, file: telegram.File):
+    if not ACTIVE:
+        return
     path = file.file_path.split('/')[-1]
     await file.download_to_drive(path)
     media_ids = upload_media([path])
@@ -65,11 +74,15 @@ async def tweet_file_2(update: Update):
 
 
 async def tweet_file_3(text: str, path: str):
+    if not ACTIVE:
+        return
     media_id = api.media_upload(path)
     create_tweet(text=text, media_ids=[media_id.media_id])
 
 
 async def tweet_files(context: CallbackContext, text: str, posts: [Post]):
+    if not ACTIVE:
+        return
     upload_files = []
     for post in posts:
         file = await context.bot.get_file(post.file_id)
