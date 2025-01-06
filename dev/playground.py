3bb6cd1369dde5fd5a2a04988c8a1d625b9f6ad8
nyx <loool1912@web.de>
2025-01-06 19:29:32 +0100

    fixing twitter upload

--- dev/playground.py
+++ dev/playground.py
@@ -1,12 +1,14 @@
 import asyncio
 import traceback
+import datetime
 
-from telegram import Update
-from telegram.ext import ContextTypes
+from telegram import Update, Message, Chat, PhotoSize, LinkPreviewOptions
+from telegram.constants import ChatType, ParseMode
+from telegram.ext import ContextTypes, CallbackContext, ApplicationBuilder, PicklePersistence, Defaults
 
 import config
-import twitter
-
+from data.lang import GERMAN, ENGLISH
+from twitter import tweet_file
 
 
 async def flag_to_hashtag_test(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -43,11 +45,34 @@ async def flag_to_hashtag_test(update: Update, context: ContextTypes.DEFAULT_TYP
 
 async def local_test():
     print("run test")
+    update = Update(channel_post=Message(caption='🇱🇮 Test', channel_chat_created=False,
+                                chat=Chat(id=-1001240262412, title='🔰 Militär-News', type=  ChatType.CHANNEL ,
+                                username='militaernews'),
+           date=datetime.datetime(2025, 1, 6, 3, 51, 43, tzinfo=datetime.timezone.utc), delete_chat_photo=False,
+           group_chat_created=False, message_id=30770, photo=(
+        PhotoSize(file_id='AgACAgIAAx0CSeznDAACeC9ne1K2Zwi2lQdvZx3ipYGs4OW43QACnewxG-Cy2Uss4Qw5VMC-AgEAAwIAA3MAAzYE',
+                  file_size=1049, file_unique_id='AQADnewxG-Cy2Ut4', height=51, width=90),
+        PhotoSize(file_id='AgACAgIAAx0CSeznDAACeC9ne1K2Zwi2lQdvZx3ipYGs4OW43QACnewxG-Cy2Uss4Qw5VMC-AgEAAwIAA20AAzYE',
+                  file_size=14082, file_unique_id='AQADnewxG-Cy2Uty', height=180, width=320),
+        PhotoSize(file_id='AgACAgIAAx0CSeznDAACeC9ne1K2Zwi2lQdvZx3ipYGs4OW43QACnewxG-Cy2Uss4Qw5VMC-AgEAAwIAA3gAAzYE',
+                  file_size=53587, file_unique_id='AQADnewxG-Cy2Ut9', height=449, width=800),
+        PhotoSize(file_id='AgACAgIAAx0CSeznDAACeC9ne1K2Zwi2lQdvZx3ipYGs4OW43QACnewxG-Cy2Uss4Qw5VMC-AgEAAwIAA3kAAzYE',
+                  file_size=94859, file_unique_id='AQADnewxG-Cy2Ut-', height=718, width=1280)),
+           sender_chat=Chat(id=-1001240262412, title='🔰 Militär-News', type=  ChatType.CHANNEL ,
+           username='militaernews'), supergroup_chat_created = False), update_id = 171625171)
 
-    with open("./res/img/argu.jpg","rb") as f1:
-        with open("./res/img/disso.jpg","rb") as f2:
-       #     print(f1.read(),f2.read())
-           # res=  upload_media([f1,f2], twitter.api_DE)
-            print("res")
+    app = (ApplicationBuilder().token(config.TOKEN)
+           .defaults(Defaults(parse_mode=ParseMode.HTML, link_preview_options=LinkPreviewOptions(is_disabled=True)))
+           .persistence(PicklePersistence(filepath="persistence"))
+           .read_timeout(50).get_updates_read_timeout(50)
+           .build())
 
-asyncio.get_event_loop().run_until_complete(local_test())
+   # await tweet_file(None, None,"Test", ENGLISH.lang_key,"./res/en/breaking.png")
+    await tweet_file(update, CallbackContext(app), "Test2", ENGLISH.lang_key)
+
+import contextlib
+if __name__ == '__main__':
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    with contextlib.suppress(KeyboardInterrupt):
+        asyncio.run(local_test())
--- twitter.py
+++ twitter.py
@@ -1,22 +1,23 @@
+import base64
 import logging
 import os
-from typing import Optional, List, IO, Union, Tuple
-import base64
-import telegram
+from pathlib import Path
+from typing import Optional, List, IO, Tuple
+
 from dotenv import load_dotenv
 from lxml.html import fromstring
 from pytwitter import Api
+from telegram import Update
 from telegram.ext import CallbackContext
-from typing_extensions import BinaryIO
 
 from data.db import Post
-from data.lang import ENGLISH
-from twitter_uploader import  TelegramTwitterTransfer
+from data.lang import ENGLISH, GERMAN
+from twitter_uploader import TelegramTwitterTransfer
 
 load_dotenv()
 
 
-def create_instance(consumer_key: str, consumer_secret: str, access_token: str, access_secret: str)-> tuple[
+def create_instance(consumer_key: str, consumer_secret: str, access_token: str, access_secret: str) -> tuple[
     Api, TelegramTwitterTransfer]:
     api = Api(
         consumer_key=consumer_key,
@@ -25,7 +26,7 @@ def create_instance(consumer_key: str, consumer_secret: str, access_token: str, 
         access_secret=access_secret
     )
     uploader = TelegramTwitterTransfer(api)
-    return api,uploader
+    return api, uploader
 
 
 api_DE, uploader_DE = create_instance(
@@ -35,7 +36,7 @@ api_DE, uploader_DE = create_instance(
     os.getenv("ACCESS_SECRET_DE"),
 )
 
-api_EN,uploader_EN = create_instance(
+api_EN, uploader_EN = create_instance(
     os.getenv("CONSUMER_KEY_EN"),
     os.getenv("CONSUMER_SECRET_EN"),
     os.getenv("ACCESS_KEY_EN"),
@@ -46,17 +47,14 @@ ACTIVE = True
 TWEET_LENGTH = 280
 
 
-def supply_twitter_instance(lang_key: Optional[str] = None) -> Optional[Tuple[Api,TelegramTwitterTransfer]]:
+def supply_twitter_instance(lang_key: Optional[str] = GERMAN.lang_key) -> Optional[Tuple[Api, TelegramTwitterTransfer]]:
     if not ACTIVE:
         return None
     clients = {
         ENGLISH.lang_key: (api_EN, uploader_EN),
-        None:(api_DE,uploader_DE)
+        GERMAN.lang_key: (api_DE, uploader_DE)
     }
     return clients.get(lang_key)
-
-
-
 
 
 def create_tweet(text: str, api: Api, media_ids=None, ):
@@ -67,19 +65,23 @@ def create_tweet(text: str, api: Api, media_ids=None, ):
         logging.error(f"Error when trying to post to twitter: {e}\n\ntext: {text}\n\nmedia_ids: {media_ids}")
 
 
-async def tweet_file( update,context, caption: str,lang_key: Optional[str] = None, file_path:Optional[str]=None):
+async def tweet_file(update:Update, context:CallbackContext, caption: str, lang_key: Optional[str] = None, file_path: Optional[str] = None):
     instance = supply_twitter_instance(lang_key)
+    print(instance)
     if instance is None:
         return
 
     api, uploader = instance
 
-    if file_path  is not None:
+    api.create_tweet(text="Hi!")
+
+    if file_path is not None:
         media_id = uploader.upload_image(file_path)
     else:
-        media_id = uploader.transfer_to_twitter(update,context)
+        media_id = await uploader.transfer_to_twitter(update, context)
 
-    create_tweet(caption, api, media_id)
+    print(media_id)
+    create_tweet(caption, api,[ media_id])
 
 
 def upload_media(files: List[IO], api: Api):
@@ -89,30 +91,34 @@ def upload_media(files: List[IO], api: Api):
     ]
 
 
-
-
 # todo: migrate to uploader
 async def tweet_files(context: CallbackContext, caption: str, posts: [Post], lang_key: Optional[str] = None):
-    api = supply_twitter_instance(lang_key)
-    if api is None:
+    instance = supply_twitter_instance(lang_key)
+    print(instance)
+    if instance is None:
         return
+
+    api, uploader = instance
 
     upload_files = []
     for post in posts:
         file = await context.bot.get_file(post.file_id)
         path = file.file_path.split('/')[-1]
         await file.download_to_drive(path)
-        upload_files.append(path)
+        upload_files.append(open(path,"rb"))
 
     media_ids = upload_media(upload_files, api)
     create_tweet(caption, api, media_ids)
-    for path in upload_files:
-        os.remove(path)
+ #   for file in upload_files:
+  #      os.remove(Path(file.))
 
 
 async def tweet_text(text: str, lang_key: Optional[str] = None):
-    api = supply_twitter_instance(lang_key)
-    if api is None:
+    instance = supply_twitter_instance(lang_key)
+    print(instance)
+    if instance is None:
         return
+
+    api, uploader = instance
 
     create_tweet(text, api)
