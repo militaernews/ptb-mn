cf416b508d3b40fca2488e3b407d080d26ee3adb
nyx <loool1912@web.de>
2025-06-24 23:23:18 +0200

    fixing quot pattern

--- bot/util/patterns.py
+++ bot/util/patterns.py
@@ -11,8 +11,8 @@ FLAG_EMOJI_HTMLTAG = re.compile(
     f"{FLAG_EMOJI.pattern}|{PATTERN_HTMLTAG.pattern}", re.IGNORECASE
 )
 
-QUOT_PATTERN = re.compile(r'& quot;',re.IGNORECASE)
-AMP_PATTERN = re.compile(r'& amp;',re.IGNORECASE)
+QUOT_PATTERN = re.compile(r'[^\S\n\r]?& quot;[^\S\n\r]?',re.IGNORECASE)
+AMP_PATTERN = re.compile(r'[^\S\n\r]?& amp;[^\S\n\r]?',re.IGNORECASE)
 
 PLACEHOLDER: Final[str] = "║"
 PATTERN_COMMAND: Final[str] = r"^\/([^@\s]+)@?(?:(\S+)|)\s?([\s\S]*)$"
