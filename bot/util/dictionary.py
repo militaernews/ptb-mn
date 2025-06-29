0138d4977f89989afe1cb77176716051741d3aef
nyx <loool1912@web.de>
2025-06-29 21:32:48 +0200

    fixes, dependency bump

--- bot/util/dictionary.py
+++ bot/util/dictionary.py
@@ -2,10 +2,14 @@ import re
 
 REPLACEMENTS = {
     "Kyjiw": r"Kie[wv]",
+    "Lemberg": r"L[wv][io][wv]",
+    "Königsberg": r"Kaliningrad",
+    "Selenskij": r"Zelensky",
 }
 
-def replace_name(text:str) -> str:
+
+def replace_name(text: str) -> str:
     for replacement, pattern in REPLACEMENTS.items():
-        text = re.sub(pattern, replacement, text,re.IGNORECASE)
+        text = re.sub(pattern, replacement, text, re.IGNORECASE)
 
     return text
