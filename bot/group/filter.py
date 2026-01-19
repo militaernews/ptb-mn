fefec725208eec8025cf934867bb823e03ca140b
nyx <loool1912@web.de>
2026-01-19 23:12:58 +0100

    fix

--- bot/group/filter.py
+++ bot/group/filter.py
@@ -7,7 +7,7 @@ from telegram.ext import CallbackContext
 from telegram.helpers import mention_html
 
 from group.bingo import handle_bingo
-from settings.config import ADMINS, LOG_GROUP
+from bot.settings.config import ADMINS, LOG_GROUP
 
 
 def check_cas(user_id: int):
