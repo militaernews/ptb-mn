97806d22e443cefbbdbbb673f8ab084094090f96
PXNX <loool1912@web.de>
2026-03-17 13:57:38 +0100

    dependency bump

--- test/translation.py
+++ test/translation.py
@@ -9,5 +9,5 @@ async def test_flag_to_hashtag():
     result  = flag_to_hashtag("🇺🇦🏳️‍🌈 Polizei von Kiew löst einen Anti-LGBT-Protest auf.")
     print(result)
 
-    assert result is f"🇺🇦🏳️‍🌈 Polizei von Kiew löst einen Anti-LGBT-Protest auf.\n\n#LGBT #Ukraine"
+    assert result == r"🇺🇦🏳️‍🌈 Polizei von Kiew löst einen Anti-LGBT-Protest auf.\n\n#LGBT #Ukraine"
 
